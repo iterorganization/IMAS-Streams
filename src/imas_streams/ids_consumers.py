d9c5fd09e9eca469d1e5dc5c264e62533c4943b0
Maarten Sebregts <msebregts@ignitioncomputing.com>
2026-02-06 10:51:53 +0100

    Move MessageProcessor into ids consumers module
    It is probably not useful for the xarray consumer

--- src/imas_streams/ids_consumers.py
+++ src/imas_streams/ids_consumers.py
@@ -1,9 +1,44 @@
 import copy
 
+import numpy as np
 from imas.ids_toplevel import IDSToplevel
 
-from imas_streams.consumer_util import MessageProcessor
 from imas_streams.metadata import StreamingIMASMetadata
+
+
+class MessageProcessor:
+    """Logic for building data arrays from streaming IMAS messages"""
+
+    def __init__(self, metadata: "StreamingIMASMetadata"):
+        self._metadata = metadata
+        self._msg_buffer = memoryview(bytearray(metadata.nbytes))
+        readonly_view = self._msg_buffer.toreadonly()
+        dtype = "<f8"  # little-endian IEEE-754 64-bits floating point number
+        self._array_view = np.frombuffer(readonly_view, dtype=dtype)
+
+    def get_array_views(self) -> list[np.ndarray]:
+        """Get list of readonly array views of the streamed data.
+        Each item in the list corresponds to the entry in metadata.dynamic_data with the
+        same index.
+        """
+        array_views = []
+        idx = 0
+        for dyndata in self._metadata.dynamic_data:
+            assert dyndata.data_type == "f64"
+            n = np.prod(dyndata.shape, dtype=int)
+            dataview = self._array_view[idx : idx + n].reshape(dyndata.shape)
+            array_views.append(dataview)
+            idx += n
+        return array_views
+
+    def set_data(self, data: bytes | bytearray) -> None:
+        """Update array views with data from a new message."""
+        if len(data) != len(self._msg_buffer):
+            raise ValueError(
+                f"Unexpected size of data: {len(data)}. "
+                f"Was expecting {len(self._msg_buffer)}."
+            )
+        self._msg_buffer[:] = data
 
 
 class StreamingIDSConsumer:
@@ -26,7 +61,9 @@ class StreamingIDSConsumer:
                 ...
     """
 
-    def __init__(self, metadata: StreamingIMASMetadata, *, return_copy: bool = True):
+    def __init__(
+        self, metadata: StreamingIMASMetadata, *, return_copy: bool = True
+    ) -> None:
         """Consumer of streaming IMAS data which outputs IDSs.
 
         Args:
