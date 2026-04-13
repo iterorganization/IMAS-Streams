04fdc326c67e2248147553527225109dd14a3a00
Maarten Sebregts <msebregts@ignitioncomputing.com>
2026-04-13 12:10:19 +0200

    Update test for loading ymmsl config

--- tests/test_muscle3.py
+++ tests/test_muscle3.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+
+import pytest
 import ymmsl
 from ymmsl.v0_2 import Configuration, Reference, resolve
+
+from imas_streams.muscle3_datasource import DATA_SOURCE
 
 ymmsl_config = """
 ymmsl_version: v0.2
@@ -26,7 +31,25 @@ resources:
 """
 
 
+@pytest.mark.xfail(
+    tuple(map(int, ymmsl.__version__.split(".")[:3])) < (0, 15, 1),
+    reason="Test needs YMMSL Entry Points plugins",
+)
 def test_load_ymmsl_config():
+    config = ymmsl.load_as(Configuration, ymmsl_config)
+    resolve(Reference([]), config)
+    config.check_consistent()
+
+
+def test_load_ymmsl_config_from_ymmsl_path(
+    tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+):
+    # Prepare yMMSL path
+    ymmsl_file = tmp_path / "imas_streams" / "data_source.ymmsl"
+    ymmsl_file.parent.mkdir(parents=True, exist_ok=True)
+    ymmsl_file.write_text(DATA_SOURCE)
+    monkeypatch.setenv("YMMSL_PATH", str(tmp_path))
+
     config = ymmsl.load_as(Configuration, ymmsl_config)
     resolve(Reference([]), config)
     config.check_consistent()
